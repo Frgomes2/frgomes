5b12ec2f56d00649dfd980765aabb484060b53fe
frgomes <flavio.raphael@msn.com>
2025-03-17 22:29:38 -0300

    Corrigindo config de assets no ambiente de produção

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,12 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  config.assets.compile = true
+  config.assets.digest = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.assets.css_compressor = nil # Evita problemas com Sass
+
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
