62abc1f51ee78599bbab2395aee9eb6440d90fe3
frgomes <flavio.raphael@msn.com>
2025-03-17 22:29:10 -0300

    Corrigindo erro de assets no Render

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
+
 # Instala dependências
 bundle install
 
-# Configurações para evitar falhas na compilação de assets
+# Configurações para evitar erro na compilação de assets
 export NODE_OPTIONS="--max_old_space_size=2560"
-export RAILS_SERVE_STATIC_FILES=true
 export RAILS_ENV=production
+export RAILS_SERVE_STATIC_FILES=true
+export RAILS_LOG_TO_STDOUT=true
 
 # Rodar migrações do banco
 bundle exec rails db:migrate
 
-# Compilar os assets sem limpar
+# Compilar os assets SEM limpar antes
 bundle exec rake assets:precompile --trace
